46339665a6e67e84f87258f5b3a325d7b5d9e559
beegitcode <mmc.makowski@gmail.com>
2018-02-08 14:41:32 +0100

    Put books into the Array.

--- src/data/Book.java
+++ src/data/Book.java
@@ -2,49 +2,82 @@ package data;
 
 public class Book {
 
-		private String title;
-	    private String author;
-	    int releaseDate;
-	    int pages;
-	    String publisher;
-	    String isbn;
+	private String title;
+	private String author;
+	private int releaseDate;
+	private int pages;
+	private String publisher;
+	private String isbn;
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
 	
-	    
-		public String getTitle() {
-			return title;
-		}
+	
 
-		public void setTitle(String title) {
-			this.title = title;
-		}
+	public int getReleaseDate() {
+		return releaseDate;
+	}
 
-		public Book(String title, String author, int releaseDate, int pages, String publisher, String isbn) {
-			
-			this.setTitle(title);
-			this.setAuthor(author);
-			this.releaseDate = releaseDate;
-			this.pages = pages;
-			this.publisher = publisher;
-			this.isbn = isbn;
-		}
-		
-		Book(Book book){
-			this(book.getTitle(), book.getAuthor(), book.releaseDate, book.pages, book.publisher, book.isbn);
-		}
+	public void setReleaseDate(int releaseDate) {
+		this.releaseDate = releaseDate;
+	}
 
+	public int getPages() {
+		return pages;
+	}
 
-		public void printInfo() {
-			String info = getTitle() + "; " + getAuthor() + "; " + releaseDate + "; " + pages + "; " + publisher
-	                + "; " + isbn;
-	        System.out.println(info);
-			
-		}
+	public void setPages(int pages) {
+		this.pages = pages;
+	}
 
-		public String getAuthor() {
-			return author;
-		}
+	public String getPublisher() {
+		return publisher;
+	}
 
-		public void setAuthor(String author) {
-			this.author = author;
-		}
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	public Book(String title, String author, int releaseDate, int pages, String publisher, String isbn) {
+
+		this.setTitle(title);
+		this.setAuthor(author);
+		this.setReleaseDate(releaseDate);
+		this.setPages(pages);
+		this.setPublisher(publisher);
+		this.setIsbn(isbn);
+	}
+
+	Book(Book book) {
+		this(book.getTitle(), book.getAuthor(), book.getReleaseDate(), book.getPages(), book.getPublisher(), book.getIsbn());
+	}
+
+	public void printInfo() {
+		String info = getTitle() + "; " + getAuthor() + "; " + getReleaseDate() + "; " + getPages() + "; " + getPublisher() + "; "
+				+ getIsbn();
+		System.out.println(info);
+
+	}
+
 }
