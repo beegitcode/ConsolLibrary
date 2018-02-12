4b5bb72ed47a47928e3d315d9b73790c0d6e496c
beegitcode <mmc.makowski@gmail.com>
2018-02-12 21:02:56 +0100

    Channged class Library - now main task  is to keep Books. Added LibraryApp and LibraryControl clases to divie tasks

---
+++ src/data/Library.java
@@ -0,0 +1,44 @@
+package data;
+
+import java.util.Iterator;
+
+import utils.DataReader;
+
+public class Library {
+
+	public final int maxBooks = 1000;
+	private Book[] books;
+	private int booksNumber;
+
+	private Library() {
+		books = new Book[maxBooks];
+	}
+
+	public Book[] getBooks() {
+		return books;
+	}
+
+	public int getBooksNumber() {
+		return booksNumber;
+	}
+
+	public void addBook(Book buk) {
+		if (booksNumber < maxBooks) {
+			books[booksNumber] = buk;
+			booksNumber++;
+		} else {
+			System.out.println("Brak miejsca w systemie");
+		}
+		
+	}
+	
+	public void printBooks() {
+		if (booksNumber == 0 ) {
+			System.out.println("Brak ksi¹¿ek w systemie");
+		}
+	
+}
+		
+	}
+
+
