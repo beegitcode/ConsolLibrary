9f098dc9a7b1ee624f527f87330e1e69726e940e
beegitcode <mmc.makowski@gmail.com>
2018-03-20 21:13:41 +0100

    Added removePublication() method and changed addPublication() method. Used  simple array list patern.

--- src/data/Library.java
+++ src/data/Library.java
@@ -1,26 +1,25 @@
 package data;
 
 import java.io.Serializable;
+import java.util.Arrays;
 
-public class Library implements Serializable{
+public class Library implements Serializable {
 
 	private static final long serialVersionUID = -1942727499318789886L;
 
-	public static final int MAX_PUBLICATIONS = 2000;
-
+	public static final int INITIAL_CAPACITY = 1;
 	private Publication[] publications;
 	private int publicationNumber;
 
-	
-
 	public Library() {
 
-		setPublications(new Publication[MAX_PUBLICATIONS]);
+		setPublications(new Publication[INITIAL_CAPACITY]);
 	}
 
 	public void addPublication(Publication publicat) throws ArrayIndexOutOfBoundsException {
-		if (getPublicationNumber() == MAX_PUBLICATIONS) {
-			throw new ArrayIndexOutOfBoundsException("MAX_PUBLICATIONS " + MAX_PUBLICATIONS);
+		if (publicationNumber == publications.length) {
+
+			publications = Arrays.copyOf(publications, publications.length * 2);
 		}
 		publications[publicationNumber] = publicat;
 		publicationNumber++;
@@ -33,6 +32,28 @@ public class Library implements Serializable{
 
 	public void addBook(Book boook) {
 		addPublication(boook);
+
+	}
+
+	public void removePublication(Publication pub) {
+		if (pub == null) {
+			return;
+		}
+		final int NOT_FOUND = -1;
+		int found = NOT_FOUND;
+		int i = 0;
+		while (i < publications.length && found == NOT_FOUND) {
+			if (pub.equals(publications[i])) {
+				found = i;
+			} else {
+				i++;
+			}
+		}
+
+		if (found != NOT_FOUND) {
+			System.arraycopy(publications, found + 1, publications, found, publications.length - found - 1);
+			publicationNumber--;
+		}
 
 	}
 
@@ -51,6 +72,5 @@ public class Library implements Serializable{
 	public void setPublicationNumber(int publicationNumber) {
 		this.publicationNumber = publicationNumber;
 	}
-
 
 }
